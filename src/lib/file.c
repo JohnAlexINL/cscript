8c6f26726331b872133babc75405ca9ace99ec6b
John Alex <bloodroserecords@gmail.com>
2025-03-13 06:55:04 -0400

    file_read to RO permissions

--- src/lib/file.c
+++ src/lib/file.c
@@ -8,14 +8,14 @@ i64 file_size(char *filename) {
 }
 
 int file_read(char *filename, char *buffer, i64 max) {
-    const i32 flags_rw = 2; const i32 modes = 0;
+    const i32 flags_ro = 0; const i32 modes = 0;
     i64 filesize = file_size(filename);
     if ( filesize <= 0 ) { print("file_read: file nil sized\n"); return -1; }
     if ( filesize >= max ) { filesize = max-1; } // keep within bounds 
-    u32 fd = open(filename, flags_rw, modes); if ( fd == -1 ) { return -2; }
+    u32 fd = open(filename, flags_ro, modes); if ( fd == -1 ) { return -2; }
     i64 total_read = 0; // if we can't do it in one pass, keep trying
     while (total_read < filesize) {
-        i32 rsize = read(fd, buffer, filesize - total_read); 
+        i32 rsize = read(fd, buffer + total_read, filesize - total_read); 
         if (rsize <= 0) {  close(fd); return -3; }
         total_read += rsize;
     }   close(fd); *((char *)buffer + filesize) = '\0'; return total_read;
